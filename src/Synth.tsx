a07ecb60bad447a11625a41a727a3252bd25b702
Nick Matantsev <nick.matantsev@gmail.com>
2022-11-13 18:37:15 -0500

    Set up basic FM synth

--- src/Synth.tsx
+++ src/Synth.tsx
@@ -1,38 +1,17 @@
 import * as Tone from 'tone';
-import { RPolySynth } from 'react-tone-rack';
+import { createRackableInstrument, RecursivePartial } from 'react-tone-rack';
+
+const RFMSynth = createRackableInstrument<
+  RecursivePartial<Tone.FMSynthOptions>
+>(Tone.MonoSynth);
 
 export const Synth: React.FC = () => {
   return (
-    <RPolySynth
+    <RFMSynth
       notes="exampleSynth"
       duration="4n"
-      voice={Tone.MonoSynth}
       volume={-1}
-      options={{
-        oscillator: {
-          type: 'fatsawtooth',
-          count: 3,
-          spread: 10,
-        },
-        envelope: {
-          attack: 0.05,
-          decay: 0.5,
-          sustain: 0.9,
-          release: 0.1,
-        },
-        filter: {
-          type: 'bandpass',
-          Q: 1.8,
-        },
-        filterEnvelope: {
-          baseFrequency: 'C3',
-          octaves: 4,
-          attack: 0.01,
-          decay: 0.3,
-          sustain: 0,
-          release: 0.2,
-        },
-      }}
+      modulationIndex={0.3}
     />
   );
 };
