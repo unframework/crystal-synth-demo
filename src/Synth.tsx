569cac651eb07bee864baa02e9c8f5e38225fcb9
Nick Matantsev <nick.matantsev@gmail.com>
2022-11-13 20:09:59 -0500

    Add tonebar component

--- src/Synth.tsx
+++ src/Synth.tsx
@@ -5,26 +5,46 @@ const RFMSynth = createRackableInstrument<
   RecursivePartial<Tone.FMSynthOptions>
 >(Tone.FMSynth);
 
+const RSynth = createRackableInstrument<RecursivePartial<Tone.SynthOptions>>(
+  Tone.Synth
+);
+
 export const Synth: React.FC = () => {
   return (
-    <RFMSynth
-      notes="exampleSynth"
-      duration="4n"
-      volume={-1}
-      modulationIndex={50}
-      harmonicity={18}
-      modulationEnvelope={{
-        attack: 0,
-        decay: 0,
-        sustain: 1,
-        release: 0,
-      }}
-      envelope={{
-        attack: 0.01,
-        decay: 0.5,
-        sustain: 0,
-        release: 0,
-      }}
-    />
+    <>
+      <RFMSynth
+        notes="exampleSynth"
+        duration="4n"
+        volume={-1}
+        modulationIndex={30}
+        harmonicity={18}
+        modulationEnvelope={{
+          attack: 0,
+          decay: 0,
+          sustain: 1,
+          release: 0,
+        }}
+        envelope={{
+          attack: 0.01,
+          decay: 0.5,
+          sustain: 0,
+          release: 0,
+        }}
+      />
+      <RSynth
+        notes="exampleSynth"
+        duration="4n"
+        volume={0}
+        oscillator={{
+          type: 'sine', // tonebar vibration
+        }}
+        envelope={{
+          attack: 0.02, // slightly slower start
+          decay: 0.5, // very slow decay
+          sustain: 0.2,
+          release: 0.3,
+        }}
+      />
+    </>
   );
 };
